6005ea9f113ac4509752be894e3c501ea426d3f5
jianfei.zhang <zjf_0731@163.com>
2023-07-12 10:27:42 +0800

    Initial commit

---
+++ .vuepress/config.ts
@@ -0,0 +1,56 @@
+import { defineUserConfig } from "vuepress";
+import recoTheme from "vuepress-theme-reco";
+
+export default defineUserConfig({
+  title: "falser101's blog",
+  description: "falser101's blog",
+  theme: recoTheme({
+    style: "@vuepress-reco/style-default",
+    logo: "/logo.png",
+    author: "falser101",
+    authorAvatar: "/head.png",
+    docsRepo: "https://github.com/vuepress-reco/vuepress-theme-reco-next",
+    docsBranch: "main",
+    docsDir: "example",
+    lastUpdatedText: "",
+    // 自动设置分类
+    autoSetBlogCategories: true,
+    // 自动将分类和标签添加至头部导航条
+    autoAddCategoryToNavbar: {
+      location: 1, // 默认 0
+      categoryText: '分类', // 默认 categories
+      tagText: '标签' // 默认 tags
+    },
+    // series 为原 sidebar
+    series: {
+      "/docs/theme-reco/": [
+        {
+          text: "module one",
+          children: ["home", "theme"],
+        },
+        {
+          text: "module two",
+          children: ["api", "plugin"],
+        },
+      ],
+    },
+    navbar: [
+      { text: "首页", link: "/" },
+      {
+        text: "文章",
+        children: [],
+      },
+    ],
+    commentConfig: {
+      type: 'giscus',
+      options: {
+        repo: 'falser101/blog',
+        repoId: 'R_kgDOJ6KYZg',
+        category: 'Announcements',
+        categoryId: 'DIC_kwDOJ6KYZs4CXzs8',
+        lang: 'zh-CN'
+      },
+    },
+  }),
+  // debug: true,
+});
