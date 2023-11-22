6754998a1108beb9e763de82595fee5904e4a78f
T2816 <zhangjf@tongtech.com>
2023-11-22 14:23:45 +0800

    base

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/falser/",
 
   lang: "zh-CN",
   title: "飞哥与小佛",
